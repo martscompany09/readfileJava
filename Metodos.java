2c8c27b4a07e5563c56aa11b9e519f41df65c6d4
martscompany09 <44786648+martscompany09@users.noreply.github.com>
2018-11-05 15:29:50 -0500

    Create Metodos.java

---
+++ Metodos.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Metodos {
+    
+    public String lecFile(String ruta) throws FileNotFoundException, IOException{        
+        BufferedReader bf = new BufferedReader(new FileReader(ruta));
+        String linea,lectura="";
+        while((linea=bf.readLine())!=null){
+            lectura+=linea;
+        }
+        return lectura;
+    }
+}
