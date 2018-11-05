261c98e0ceefda4ac7cc4f9c8807c85f28dcce3b
martscompany09 <44786648+martscompany09@users.noreply.github.com>
2018-11-05 15:32:30 -0500

    Metodos.java

--- Metodos.java
+++ Metodos.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 
 public class Metodos {
     
-    public String lecFile(String ruta) throws FileNotFoundException, IOException{        
+    public String lecFile(String ruta) throws FileNotFoundException, IOException{
+        //CARGAR Y LEER ARCHIVO
         BufferedReader bf = new BufferedReader(new FileReader(ruta));
+        //DECLARO VARIABLES
         String linea,lectura="";
+        //RECORRO LINEAS
         while((linea=bf.readLine())!=null){
             lectura+=linea;
         }
+        //RETORNO DATO
         return lectura;
     }
 }
